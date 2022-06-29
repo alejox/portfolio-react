68d8a2403bdf3246105c75eb8e1e8e2d1bc907b4
alejox <alejoxdrums@gmail.com>
2022-06-29 00:51:54 -0500

    contact

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,53 +1,57 @@
-import React from 'react';
-import './about.css';
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
-
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
- return (
-  <section id='about'>
-   <h5>Get To Know</h5>
-   <h2>About me</h2>
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
 
-   <div className='container about__container'>
-    <div className='about__me'>
-     <div className='about__me-image'>
-      <img src={ME} alt='About Image' />
-     </div>
-    </div>
-    <div className='about__content'>
-      <div className="about__cards">
-        <article className="about__card">
-          <FaAward className='about__icon'/>
-          <h5>Experience</h5>
-          <small>6+ Months Working</small>
-        </article>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>6+ Months Working</small>
+            </article>
 
-        <article className="about__card">
-          <FiUsers className='about__icon'/>
-          <h5>Clients</h5>
-          <small>200+ Worldwide</small>
-        </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
 
-        <article className="about__card">
-          <VscFolderLibrary className='about__icon'/>
-          <h5>Projects</h5>
-          <small>80+ Completed projects</small>
-        </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            mollitia reiciendis voluptatum, ut ipsum ea dolores, neque culpa eum
+            corrupti, consequuntur aliquid pariatur atque rerum? Dolores error
+            ducimus vel maiores!
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
+        </div>
       </div>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam mollitia reiciendis voluptatum, ut ipsum ea dolores, neque culpa eum corrupti, consequuntur aliquid pariatur atque rerum? Dolores error ducimus vel maiores!
-      </p>
-
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-    </div>
-   </div>
-  </section>
- );
+    </section>
+  );
 };
 
 export default About;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,38 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar:AVTR1,
+    name:'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque at cum vitae nobis, aliquid modi. Est facere harum corrupti, totam culpa beatae molestias nihil laborum eum eius porro! Itaque.'
+  },
+  {
+    avatar:AVTR2,
+    name:'Shatta Wale',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque at cum vitae nobis, aliquid modi. Est facere harum corrupti, totam culpa beatae molestias nihil laborum eum eius porro! Itaque.'
+  },
+  {
+    avatar:AVTR3,
+    name:'Kwame Despite',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque at cum vitae nobis, aliquid modi. Est facere harum corrupti, totam culpa beatae molestias nihil laborum eum eius porro! Itaque.'
+  },
+  {
+    avatar:AVTR4,
+    name:'Nana Ama McBrown',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloremque at cum vitae nobis, aliquid modi. Est facere harum corrupti, totam culpa beatae molestias nihil laborum eum eius porro! Itaque.'
+  },
+]
+
 
 const Testimonials = () => {
   return (
@@ -12,44 +44,27 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One"/>
-          </div>
-            <h5>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In fuga aut quibusdam repudiandae nesciunt dignissimos sequi vitae. Tenetur facere inventore consequatur at nesciunt tempora atque, sit nemo quod, qui enim.
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One"/>
-          </div>
-            <h5>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In fuga aut quibusdam repudiandae nesciunt dignissimos sequi vitae. Tenetur facere inventore consequatur at nesciunt tempora atque, sit nemo quod, qui enim.
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One"/>
-          </div>
-            <h5>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In fuga aut quibusdam repudiandae nesciunt dignissimos sequi vitae. Tenetur facere inventore consequatur at nesciunt tempora atque, sit nemo quod, qui enim.
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One"/>
-          </div>
-            <h5>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. In fuga aut quibusdam repudiandae nesciunt dignissimos sequi vitae. Tenetur facere inventore consequatur at nesciunt tempora atque, sit nemo quod, qui enim.
-            </small>
-        </article>
-      </div>
+      <Swiper className="container testimonials__container "
+      // install Swiper modules
+      modules={[ Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index)=>{
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} alt="" />
+                </div>
+
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }    
+      </Swiper>
     </section>
   )
 }
